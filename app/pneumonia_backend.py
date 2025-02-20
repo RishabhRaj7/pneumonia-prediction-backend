1f282b91818d1a41babba351aa0fa50c604b365d
RishabhRaj7 <rishabh.raj37@gmail.com>
2025-02-20 21:24:58 +0530

    Fix CORS

--- app/pneumonia_backend.py
+++ app/pneumonia_backend.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import tensorflow as tf
 import numpy as np
-import cv2
 import os
 import logging
 from werkzeug.utils import secure_filename
